279f82c92dd31361f6cb45019341518785e47126
cokeeh <103632425+cokeeh@users.noreply.github.com>
2022-07-15 16:23:24 +0900

    Delete kb directory

--- kb/js/best.js
+++
@@ -1,7 +0,0 @@
-		//*베스트셀러 멀티 페이지*//
-		$(function(){
-			$("#best_seller_book > .title > ul > li").mouseenter(function(){
-				$(".best_seller_list > article ").stop().hide();
-				$(".best_seller_list > article ").eq($(this).index()).stop().show();
-			});
-		});
--- kb/js/newbook.js
+++
@@ -1,7 +0,0 @@
-	//*화제의 신간 멀티페이지*//
-	$(function(){
-		$("#newbook > .title > ul > li").mouseenter(function(){
-			$(".newbook_list > article ").stop().hide();
-			$(".newbook_list > article ").eq($(this).index()).stop().show();
-		});
-	});
